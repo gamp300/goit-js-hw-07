06feb09d446ddbb8e922bb84f0a2d7185e7b0ccb
gamp300 <49374048+gamp300@users.noreply.github.com>
2023-10-24 22:26:33 -0500

    Update1 7.1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+console.log(galleryItems[0].preview);
+
+galleryItems.forEach((element) => {
+  const item = document.createElement("li");
+  gallery.append(item);
+  item.classList.add("gallery__item");
+
+  const conect = document.createElement("a");
+  item.append(conect);
+  conect.classList.add("gallery__link");
+  conect.setAttribute("href", element.original);
+
+  const image = document.createElement("img");
+  conect.append(image);
+  image.classList.add("gallery__image");
+  image.setAttribute("src", element.preview);
+  image.setAttribute("data-source", element.original);
+  image.setAttribute("alt", element.description);
+});
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  console.log(event.target);
+});
